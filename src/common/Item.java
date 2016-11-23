4e51103b5cdde28552c0a4bd87656379410ce2a5
Johan Mickos <jmickos@bu.edu>
2016-11-23 16:58:23 +0100

    Add builder to Item, slight updates

--- src/common/Item.java
+++ src/common/Item.java
@@ -1,12 +1,47 @@
 package common;
 
+import java.util.UUID;
+
 public class Item {
     private String id;
     private String name;
     private Double price;
-    private ItemType type;
+    private Category category;
 
-    public enum ItemType {
-        Something
+    public enum Category {
+        Camera,
+        Furniture,
+        Electronics,
+        Miscellaneaous,
     }
+
+    private Item() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public static Builder builder() {
+        return new Item.Builder();
+    }
+
+
+    public static class Builder {
+        private Item instance = new Item();
+
+        public Builder name(String name) {
+            instance.name = name;
+            return this;
+        }
+        public Builder price(Double price) {
+            instance.price = price;
+            return this;
+        }
+        public Builder category(Category category) {
+            instance.category = category;
+            return this;
+        }
+        public Item build() {
+            return instance;
+        }
+    }
+
 }
