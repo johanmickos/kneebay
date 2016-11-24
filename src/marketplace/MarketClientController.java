76ce8ffb3a827f52c73f1e971c7fe90a6c1b6ce4
Johan Mickos <jmickos@bu.edu>
2016-11-24 13:52:25 +0100

    Client: bubble events into UI, support Create/Buy, refactor

--- src/marketplace/MarketClientController.java
+++ src/marketplace/MarketClientController.java
@@ -9,12 +9,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.Callback;
 import marketplace.gui.models.ItemModel;
 import marketplace.rmi.MarketClientImpl;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.Naming;
@@ -38,54 +46,92 @@ public class MarketClientController implements Initializable {
     public static final int INITIAL_FUNDS = 1000;
 
     private MarketClientImpl client;
+
     private Account account;
     private Bank bank;
-    private String bankname = Bank.DEFAULT_BANK;
-
     private Marketplace marketplace;
+
     private String marketplaceName = Marketplace.DEFAULT_MARKETPLACE;
+    private String bankName = Bank.DEFAULT_BANK;
+    private String username;
 
     private static final String DISCONNECT_STR = "Disconnect";
-    private static final String REGISTER_STR   = "\u2007Register\u2007";
+    private static final String REGISTER_STR = "\u2007Register\u2007";
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         registerButton.setText(REGISTER_STR);
+        initializeMarketplaceTable();
+    }
 
-        /*
-                        <columns>
-                    <TableColumn prefWidth="75.0" text="Price"/>
-                    <TableColumn prefWidth="75.0" text="Title"/>
-                    <TableColumn prefWidth="75.0" text="Category"/>
-                    <TableColumn prefWidth="97.0" text="Description"/>
-                    <TableColumn prefWidth="117.0" text="ID"/>
-                </columns>
-         */
-
+    private void initializeMarketplaceTable() {
         TableColumn<ItemModel, String> priceCol = new TableColumn<>("Price");
         priceCol.setMinWidth(75);
         priceCol.setCellValueFactory(new PropertyValueFactory<ItemModel, String>("price"));
 
         TableColumn<ItemModel, String> titleCol = new TableColumn<>("Title");
-        titleCol.setMinWidth(75);
+        titleCol.setMinWidth(100);
         titleCol.setCellValueFactory(new PropertyValueFactory<ItemModel, String>("name"));
 
         TableColumn<ItemModel, String> catCol = new TableColumn<>("Category");
-        catCol.setMinWidth(75);
+        catCol.setMinWidth(125);
         catCol.setCellValueFactory(new PropertyValueFactory<ItemModel, String>("category"));
 
-//
-//        TableColumn<ItemModel, String> descriptionCol = new TableColumn<>("Description");
-//        descriptionCol.setMinWidth(97);
-//        descriptionCol.setCellValueFactory(new PropertyValueFactory<ItemModel, String>("description"));
+//        TableColumn<ItemModel, String> idCol = new TableColumn<>("ID");
+//        idCol.setMinWidth(117);
+//        idCol.setCellValueFactory(new PropertyValueFactory<ItemModel, String>("id"));
 
-        TableColumn<ItemModel, String> idCol = new TableColumn<>("ID");
-        idCol.setMinWidth(117);
-        idCol.setCellValueFactory(new PropertyValueFactory<ItemModel, String>("id"));
+        TableColumn actionCol = new TableColumn("Actions");
+        catCol.setMinWidth(100);
+        Callback<TableColumn<ItemModel, String>, TableCell<ItemModel, String>> cellFactory =
+                new Callback<TableColumn<ItemModel, String>, TableCell<ItemModel, String>>() {
+                    @Override
+                    public TableCell call(final TableColumn<ItemModel, String> param) {
+                        final TableCell<ItemModel, String> cell = new TableCell<ItemModel, String>() {
 
+                            final Button buyButton = new Button("Buy");
 
-        marketplaceTable.getColumns().addAll(priceCol, titleCol, catCol, idCol);
+                            @Override
+                            public void updateItem(String item, boolean empty) {
+                                super.updateItem(item, empty);
+                                if (empty) {
+                                    setGraphic(null);
+                                    setText(null);
+                                } else {
+                                    buyButton.setOnAction((ActionEvent event) ->
+                                    {
+                                        ItemModel itemModel = getTableView().getItems().get(getIndex());
+                                        handleBuyItem(itemModel);
+                                    });
+                                    setGraphic(buyButton);
+                                    setText(null);
+                                }
+                            }
+                        };
+                        return cell;
+                    }
+                };
+        actionCol.setCellFactory(cellFactory);
 
+        marketplaceTable.getColumns().addAll(priceCol, titleCol, catCol, actionCol);
+    }
+
+    private void handleBuyItem(ItemModel itemModel) {
+        log.info("Buying item " + itemModel.getName() + " for " + username);
+        try {
+            Item clone = Item.builder()
+                    .name(itemModel.getName())
+                    .price(itemModel.getPrice())
+                    .id(itemModel.getId())
+                    .category(itemModel.getCategory())
+                    .build();
+            marketplace.buyItem(clone, username);
+            logArea.appendText("You purchased the item '" + itemModel.getName() + "' for $" + itemModel.getPrice());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            log.severe("Could not purchase item " + itemModel.getName());
+            logArea.appendText("Could not purchase the item " + itemModel.getName());
+        }
     }
 
     public void onRegisterUser(ActionEvent actionEvent) {
@@ -93,43 +139,90 @@ public class MarketClientController implements Initializable {
 
         log.info("Handling user registration for " + username);
         try {
-            client = new MarketClientImpl(username);
-
-
-            bank = (Bank) Naming.lookup(bankname);
-            // TODO Look up marketplace
-
+            client = new MarketClientImpl(username, this);
+            this.username = username;
+            bank = (Bank) Naming.lookup(bankName);
             account = bank.newAccount(username);
+            marketplace = (Marketplace) Naming.lookup(marketplaceName);
+            marketplace.register(username, null, account, client);
+
             account.deposit(INITIAL_FUNDS);
-            System.out.println("Connected to bank: " + bankname);
+
             fundsLabel.setText(String.valueOf("$" + account.getBalance()));
             usernameField.setEditable(false);
             registerButton.setText(DISCONNECT_STR);
             marketplaceLabel.setText("Marketplace: " + marketplaceName);
+
             // TODO Handle accordingly
-        } catch (RemoteException e) {
+        } catch (RemoteException | MalformedURLException | NotBoundException | RejectedException e) {
             e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (NotBoundException e) {
-            e.printStackTrace();
-        } catch (RejectedException e) {
-            e.printStackTrace();
+            registerButton.setText(REGISTER_STR);
+            registerButton.setSelected(false);
         }
     }
+
     public void onUnregisterUser(ActionEvent actionEvent) {
         // TODO Complete
         log.info("Unregistering user");
         registerButton.setText(REGISTER_STR);
         usernameField.setEditable(true);
+        try {
+            marketplace.unregister(username);
+        } catch (RemoteException e) {
+            // TODO Show exception
+            e.printStackTrace();
+        }
     }
 
-    public void onNewWish(ActionEvent actionEvent) {
+    public void onNewWish(ActionEvent actionEvent) throws IOException {
         log.info("Creating new wish");
+
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/text.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("My modal window");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initOwner(
+                ((Node) actionEvent.getSource()).getScene().getWindow());
+        stage.show();
     }
 
-    public void onNewItem(ActionEvent actionEvent) {
+    public void onNewItem(ActionEvent actionEvent) throws IOException {
         log.info("Creating new item");
+        // Show modal
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/add-item-modal.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Add New Item to " + marketplaceName);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+
+        TextField newItemTitle = (TextField) scene.lookup("#newItemTitle");
+        TextField newItemPrice = (TextField) scene.lookup("#newItemPrice");
+        ChoiceBox categoryChoice = (ChoiceBox) scene.lookup("#newItemCategory");
+        Button submitButton = (Button) scene.lookup("#submitNewItem");
+
+        categoryChoice.getItems().addAll(Item.Category.values());
+
+        submitButton.setOnAction(event -> {
+            // TODO Validate
+            Item newItem = Item.builder()
+                    .name(newItemTitle.getText())
+                    .category((Item.Category) categoryChoice.getValue())
+                    .price(Float.parseFloat(newItemPrice.getText()))
+                    .build();
+            try {
+                marketplace.addItem(newItem);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+                logArea.appendText("Could not create the new item '" + newItem.getName() + "'");
+            }
+            stage.close();
+        });
+
+        stage.show();
+
     }
 
     public void onToggleRegistration(ActionEvent actionEvent) {
@@ -146,13 +239,9 @@ public class MarketClientController implements Initializable {
         updates.add(Item.builder().name("Nikon X80").category(Item.Category.Camera).price(829).build());
         updates.add(Item.builder().name("Giant Sofa").category(Item.Category.Furniture).price(1200).build());
         updates.add(Item.builder().name("Jar of Bees").category(Item.Category.Miscellaneaous).price(9).build());
-        setMarketplaceTable(updates);
+        updateMarketplace(updates);
     }
 
-    private void setMarketplaceTable(Collection<Item> items) {
-        ObservableList<ItemModel> data = generateItemModels(items);
-        marketplaceTable.setItems(data);
-    }
 
     private ObservableList<ItemModel> generateItemModels(Collection<Item> items) {
         ObservableList<ItemModel> data = FXCollections.observableArrayList();
@@ -160,5 +249,30 @@ public class MarketClientController implements Initializable {
             data.add(new ItemModel(it.getId(), it.getName(), it.getPrice(), it.getCategory()));
         }
         return data;
+    }
+
+    public void onWishNotify(Item type) {
+        logArea.appendText("An item from your wish list is available!");
+    }
+
+    public void onItemSold(Item item) {
+
+    }
+
+    public void onItemPurchased(Item item) {
+
+    }
+
+    public void onLackOfFunds() {
+
+    }
+
+    public void updateMarketplace(Collection<Item> allItems) {
+        ObservableList<ItemModel> data = generateItemModels(allItems);
+        marketplaceTable.setItems(data);
+    }
+
+    public void onException(String data) {
+
     }
 }
--- src/marketplace/rmi/MarketClientImpl.java
+++ src/marketplace/rmi/MarketClientImpl.java
@@ -1,8 +1,8 @@
 package marketplace.rmi;
 
 import common.Item;
-import common.ItemWish;
 import common.rmi.interfaces.MarketClient;
+import marketplace.MarketClientController;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -11,44 +11,46 @@ import java.util.Collection;
 public class MarketClientImpl extends UnicastRemoteObject implements MarketClient {
 
     private final String username;
+    private final MarketClientController controller;
 
-    public MarketClientImpl(String username) throws RemoteException {
+    public MarketClientImpl(String username, MarketClientController controller) throws RemoteException {
         super();
         this.username = username;
+        this.controller = controller;
     }
 
     @Override
     public void onWishNotify(Item wish) throws RemoteException {
-
+        controller.onWishNotify(wish);
     }
 
     @Override
     public void onItemSold(Item item) throws RemoteException {
-
+        controller.onItemSold(item);
     }
 
     @Override
     public void onItemPurchased(Item item) throws RemoteException {
-
+        controller.onItemPurchased(item);
     }
 
     @Override
     public void onLackOfFunds() throws RemoteException {
-
+        controller.onLackOfFunds();
     }
 
     @Override
     public void onException(String data) throws RemoteException {
-
+        controller.onException(data);
     }
 
     @Override
     public void onException(Exception ex) throws RemoteException {
-
+        controller.onException(ex.getMessage());
     }
 
     @Override
     public void updateMarketplace(Collection<Item> allItems) throws RemoteException {
-
+        controller.updateMarketplace(allItems);
     }
 }
