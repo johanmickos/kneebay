1108c9f74e4feffe16caf2ec43de64b4ec8f2911
Johan Mickos <jmickos@bu.edu>
2016-11-30 17:26:44 +0100

    Add getters/setters for DB models

--- src/marketplace/database/models/ItemModel.java
+++ src/marketplace/database/models/ItemModel.java
@@ -1,13 +1,63 @@
 package marketplace.database.models;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Entity
 public class ItemModel {
     @Id
-    String id;
-    String buyer;  // Foreign key constraint
-    String seller;  // Foreign key constraint
-    String name;
-    float price;
-    ItemStatus status;
+    private String id;
+    private String buyer;  // Foreign key constraint
+    private String seller;  // Foreign key constraint
+    private String name;
+    private float price;
+    private ItemStatus status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(String buyer) {
+        this.buyer = buyer;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public ItemStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ItemStatus status) {
+        this.status = status;
+    }
 }
--- src/marketplace/database/models/UserModel.java
+++ src/marketplace/database/models/UserModel.java
@@ -1,14 +1,65 @@
 package marketplace.database.models;
 
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Collection;
 
+@Entity
 public class UserModel {
     @Id
-    String id;
-    String currentSession; // TODO Check if this is necessary
-    String username;
-    String password; // TODO If we care about security, just store a salted hash
-    Integer numItemsSold;
-    Integer numItemsBought;
+    private String id;
+    private String currentSession; // TODO Check if this is necessary
+    private String username;
+    private String password; // TODO If we care about security, just store a salted hash
+    private Integer numItemsSold;
+    private Integer numItemsBought;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCurrentSession() {
+        return currentSession;
+    }
+
+    public void setCurrentSession(String currentSession) {
+        this.currentSession = currentSession;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getNumItemsSold() {
+        return numItemsSold;
+    }
+
+    public void setNumItemsSold(Integer numItemsSold) {
+        this.numItemsSold = numItemsSold;
+    }
+
+    public Integer getNumItemsBought() {
+        return numItemsBought;
+    }
+
+    public void setNumItemsBought(Integer numItemsBought) {
+        this.numItemsBought = numItemsBought;
+    }
 }
